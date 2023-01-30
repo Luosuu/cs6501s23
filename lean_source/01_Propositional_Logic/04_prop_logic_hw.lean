e304ef075fdb2a0ef4485ce90de3192940083966
luosuu <zhluosuu@outlook.com>
2023-01-30 16:16:03 -0500

    pEval

--- lean_source/01_Propositional_Logic/04_prop_logic_hw.lean
+++ lean_source/01_Propositional_Logic/04_prop_logic_hw.lean
@@ -1,4 +1,3 @@
-
 inductive prop_var : Type
 | mk (n : ℕ)
 
@@ -13,6 +12,8 @@ inductive binop
 | opImp
 | opIff
 | opXor
+| opNand
+| opNor
 
 open binop
 
@@ -52,16 +53,18 @@ def an_and_expr :=
 
 def True := pLit tt
 def False := pLit ff
-
-
 notation (name := pVar) `[` v `]` :=  pVar v
 notation (name := pAnd) e1 ∧ e2 :=  pBinOp opAnd e1 e2
 notation (name := pOr) e1 ∨ e2 :=  pBinOp opOr e1 e2
 notation (name := pNot) ¬e := pUnOp opNot e
+-- notation (name := pNot) ¬e := pNot e
 notation (name := pImp) e1 => e2 := pBinOp opImp e1 e2
 notation (name := pIff) e1 ↔ e2 := pBinOp opIff e1 e2
 notation (name := pXor) e1 ⊕ e2 := pBinOp opXor e1 e2
--- notation (name := pNot) ¬e := pNot e
+notation (name := pNand) e1 ↑ e2 := pBinOp opNand e1 e2 -- nand
+notation (name := pNor) e1 × e2 := pBinOp opNor e1 e2
+
+-- notation (name := pNor) e1 ↓ e2 := pBinOp opNor e1 e2 (error: ↓ unexpected token)
 
 -- first original notation
 -- notation (name := var_mk) `[` v `]` :=  pVar v
@@ -75,7 +78,7 @@ notation (name := pXor) e1 ⊕ e2 := pBinOp opXor e1 e2
 def X := [v₀]
 def Y := [v₁]
 def Z := [v₂]
-
+#check X
 -- original variable expressions
 -- def X := [prop_var.mk 0]
 -- def Y := [prop_var.mk 1]
@@ -87,6 +90,12 @@ def e3 := ¬ Z
 def e4 := e1 => e2  -- avoid overloading →
 def e5 := e1 ↔ e1
 def e6 := X ⊕ ¬X
+def e7 := X ↑ Y
+def e8 := X × Y
+
+#reduce e7
+#reduce e8
+#check e1
 
 def bimp : bool → bool → bool
 | tt tt := tt
@@ -100,18 +109,34 @@ def biff : bool → bool → bool
 | ff tt := ff
 | ff ff := tt
 
+def bnad: bool → bool → bool
+| tt tt := ff
+| tt ff := ff
+| ff tt := ff
+| ff ff := tt
+
+def bnor: bool → bool → bool
+| tt tt := ff
+| tt ff := tt
+| ff tt := tt
+| ff ff := tt
+
 def op_sem : binop → (bool → bool → bool) -- operation semantics
 | opAnd := band
 | opOr  := bor
 | opImp := bimp
 | opIff := biff
 | opXor := bxor
+| opNand := bnad
+| opNor := bnor
 
 def unop_sem: unop → (bool → bool)
 | opNot := bnot
 
 #reduce ((op_sem opAnd) tt ff)
 #reduce (op_sem opOr tt ff)
+#reduce op_sem opNand tt ff
+#reduce op_sem opNor tt tt
 
 -- original Operational semantics
 -- def pEval : prop_expr → (prop_var → bool) → bool
@@ -127,11 +152,36 @@ def unop_sem: unop → (bool → bool)
 
 def pEval : prop_expr → (prop_var → bool) → bool
 | (pLit b)          i := b
-| ([v])             i := i v                  -- our [] notation on the left
-| (pUnOp unop e)    i := unop_sem unop (pEval e i)     -- our ¬ notation; Lean's bnot
---| (¬e)              i := bnot (pEval e i)     -- our ¬ notation; Lean's bnot
+| ([v])             i := i v                            -- our [] notation on the left
+| (pUnOp unop e)    i := unop_sem unop (pEval e i)    -- our ¬ notation; Lean's bnot
+--| (¬e)            i := bnot (pEval e i)             -- our ¬ notation; Lean's bnot
 | (pBinOp op e1 e2) i := op_sem op (pEval e1 i) (pEval e2 i) -- hw1
 -- | (pBinOp op e1 e2) i := (pEval e1 i) && (pEval e2 i) -- BUG!
 
+#reduce ¬e1
+
+def e_Nand := e1 ↑ e2
+def e_Nor := e1 × e2
+
+#reduce e_Nand
+#reduce e_Nor
+
+def all_true : prop_var → bool
+| _ := tt
+
+def all_false : prop_var → bool
+| _ := ff
+
+def mixed: prop_var → bool
+| v₀ := tt
+| v₁ := ff
+| v₂ := tt
 
 
+#reduce pEval (X ↑ Y) all_false 
+#reduce pEval (X × Y) all_true
+#reduce pEval ¬(X × Y)
+#reduce pEval True
+#reduce pEval ¬True
+#reduce pEval ((X×Y)↑(X∧Y)) all_false
+#reduce pEval ((e1×e2)↑(e1∧e2)) all_false
